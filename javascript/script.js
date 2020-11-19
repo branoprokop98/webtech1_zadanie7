dee4f440cc2c218156feca5f9a418a63266fa566
Branislav <branislavprokop98@gmail.com>
2020-11-19 17:06:33 +0100

    search

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,11 @@
 let draggedObject;
-let hoveredObject;
+let hoveredObject = -1;
 let gallery;
+let findedPictures = new Array()
 
-let positions = [2, 4, 3, 1, 0];
+let objectPosition = new Array()
+
+let positions = [0, 1, 2, 3, 4];
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -11,19 +14,58 @@ document.addEventListener("DOMContentLoaded", () => {
     const searchInput = document.getElementById("search")
     const cookieValue = getCookie("search");
     const cookiePosition = getCookie("position");
+    const searchArrayCookie = getCookie("searchArray");
     JSON.stringify(positions);
     console.log(cookieValue)
-    if(cookieValue){
+    if(searchArrayCookie){
+        findedPictures = JSON.parse(searchArrayCookie);
+    }
+    if (cookieValue) {
         searchInput.value = cookieValue;
     }
-    if(cookiePosition){
+    if (cookiePosition) {
         positions = JSON.parse(cookiePosition)
         console.log(positions);
         console.log(cookiePosition);
     }
 
-    searchInput.addEventListener("input", (evt)=>{
+    searchInput.addEventListener("input", (evt) => {
+        findedPictures.splice(0, 4)
+        fetch("resources/photos.json")
+            .then(response => response.json())
+            .then(json => {
+                positions.forEach((item, index) => {
+                    if (json.photos[item].title.includes(searchInput.value) && searchInput.value != "") {
+                        findedPictures.push(json.photos[item])
+                    }
+                })
+                if (findedPictures.length > 0 && searchInput.value != "") {
+                    clearGallery()
+                    findedPictures.forEach((item, index) => {
+                        let galleryItem = document.createElement("img");
+                        galleryItem.setAttribute("src", findedPictures[index].src)
+                        galleryItem.classList = "thumbnail"
+                        gallery.appendChild(galleryItem)
+                        galleryItem.ondragstart = dragStart
+                        galleryItem.ondragover = dragOver
+                        setCookie("searchArray", JSON.stringify(findedPictures), 2);
+                    })
+                }
+                else {
+                    clearGallery()
+                    positions.forEach((item, index) => {
+                        let galleryItem = document.createElement("img");
+                        galleryItem.setAttribute("src", json.photos[item].src)
+                        galleryItem.classList = "thumbnail"
+                        galleryItem.id = index
+                        gallery.appendChild(galleryItem)
+                        galleryItem.ondragstart = dragStart
+                        galleryItem.ondragover = dragOver
+                    });
+                }
+            })
         console.log(evt.target.value)
+        console.log(findedPictures)
         setCookie("search", evt.target.value, 2)
     })
 
@@ -35,14 +77,30 @@ document.addEventListener("DOMContentLoaded", () => {
                 galleryItem.setAttribute("src", json.photos[item].src)
                 galleryItem.classList = "thumbnail"
                 galleryItem.id = index
-                positions.push(index);
+                objectPosition[index] = galleryItem
                 gallery.appendChild(galleryItem)
                 galleryItem.ondragstart = dragStart
-
                 galleryItem.ondragover = dragOver
-            });
+                galleryItem.ondragend = dragEnd
+            })
+            if(findedPictures.length > 0 && searchInput.value != ""){
+                clearGallery()
+                findedPictures.forEach((item, index) => {
+                    let galleryItem = document.createElement("img");
+                    galleryItem.setAttribute("src", findedPictures[index].src)
+                    galleryItem.classList = "thumbnail"
+                    galleryItem.id = index
+                    gallery.appendChild(galleryItem)
+                    galleryItem.ondragstart = dragStart
+                    galleryItem.ondragover = dragOver
+                });
+            }
         })
+
+
+
     setCookie("position", JSON.stringify(positions), 2);
+    console.log(positions.toString())
 });
 
 function setCookie(cname, cvalue, exdays) {
@@ -73,7 +131,24 @@ function dragStart(evt) {
 }
 
 function dragOver(evt) {
-    hoveredObject = evt.target
+    if(evt.target.id != hoveredObject.id){
+        hoveredObject = evt.target
+        console.log(hoveredObject);
+    }
 
     gallery.insertBefore(draggedObject, hoveredObject.nextSibling)
+    console.log(draggedObject.id, hoveredObject.id);
+    //console.log(draggedObject, hoveredObject);
+}
+
+function dragEnd(evt){
+    let tmp = objectPosition[draggedObject.id]
+    objectPosition[draggedObject.id] = objectPosition[hoveredObject.id];
+    objectPosition[hoveredObject.id] = tmp;
+}
+
+function clearGallery() {
+    while (gallery.lastElementChild) {
+        gallery.removeChild(gallery.lastElementChild);
+    }
 }
